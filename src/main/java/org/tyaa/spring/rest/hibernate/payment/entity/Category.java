be8cff53c36265dba73250bb16228287d2cc825a
YuriiTrofimenko <tyaa@ukr.net>
2018-09-28 13:19:03 +0300

    init

---
+++ src/main/java/org/tyaa/spring/rest/hibernate/payment/entity/Category.java
@@ -0,0 +1,50 @@
+package org.tyaa.spring.rest.hibernate.payment.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Categories")
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = -7535596996076584656L;
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	@OneToMany(mappedBy="category")
+	private Set<Product> products;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+}
